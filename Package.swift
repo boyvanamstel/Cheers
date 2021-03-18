79362b61803c133c0a6c0a0c4f3dcf9da4e7d3a5
Boy van Amstel <boy@boyvanamstel.nl>
2021-03-18 22:18:36 +0100

    Include macOS sources in SPM

--- Package.swift
+++ Package.swift
@@ -3,14 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "Cheers",
-    // platforms: [.iOS("8.0"), .macOS("10.13"), .tvOS("9.2")],
     products: [
         .library(name: "Cheers", targets: ["Cheers"])
     ],
     targets: [
         .target(
             name: "Cheers",
-            path: "Sources/Common"
+            path: "Sources/"
         )
     ]
 )
